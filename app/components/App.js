3d9f7a779fe4fae887ea0f73e90978212d1dac6e
Theodore <theo@gmail.com>
2015-07-04 00:53:30 -0700

    added stylesheet and peerid emitter

--- app/components/App.js
+++ app/components/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 export let __hotReload = true
 
-window.mv;
 // Compatibility shim
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 var peer = new Peer({ key: 'vhlr16yw7jfqd7vi', debug: 3, config: {'iceServers': [
@@ -29,15 +28,23 @@ var App = React.createClass({
         var fail = () => console.log('failed')
 
         navigator.getUserMedia({audio: true, video: true}, succ, fail) 
+
+        peer.on('open', () => {
+            this.setState({peerId: peer.id})
+        })
     },
 
     render: function() {
+        var peerIdBlock = this.state.peerId ? this.state.peerId: ''
+
         return (
             <div>
-              Hello World
               <div className='video-container'>
                 <video className='their-video' autoplay></video>
                 <video className='my-video' muted='true' autoPlay ref='myVideo' src={this.state.src}></video>
+              </div>
+              <div>
+                {peerIdBlock}
               </div>
             </div>
          ) 
