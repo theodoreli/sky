539cd64aee911db3191450d862cbdcebb464300c
Theodore <theo@gmail.com>
2015-07-04 01:55:42 -0700

    working call

--- app/components/App.js
+++ app/components/App.js
@@ -9,14 +9,14 @@ var peer = new Peer({ key: 'vhlr16yw7jfqd7vi', debug: 3, config: {'iceServers': 
 ]}});
 
 var App = React.createClass({
-    getInitialState: function() {
+    getInitialState() {
         return {
-            src: ''
+            src: '',
+            peerTarget: '',
         }
     },
 
-    componentDidMount: function()  {
-        console.log('did mount')
+    componentDidMount()  {
         var succ = (stream) => {
             //var myVideo = React.findDOMNode(this.refs.myVideo)
             //console.log(myVideo)
@@ -32,19 +32,55 @@ var App = React.createClass({
         peer.on('open', () => {
             this.setState({peerId: peer.id})
         })
+
+        peer.on('call', (call) => {
+          // Answer the call automatically (instead of prompting user) for demo purposes
+          call.answer(window.localStream);
+          this._connect(call);
+        });
     },
 
-    render: function() {
+    _changeField: function(event) {
+        this.setState({peerTarget: event.currentTarget.value})
+    },
+
+    _connectHandler(e) {
+        console.log('i was clicked')
+        var call = peer.call(this.state.peerTarget, window.localStream) 
+        this._connect(call)
+    },
+
+    _connect(call) {
+        if (window.existingCall) { window.existingCall.close()}
+
+        call.on('stream', (stream) => {
+          this.setState({theirVideo: URL.createObjectURL(stream)})
+        });
+
+        window.existingCall = call;
+    },
+
+    render() {
+        var renderConnect = () => {
+            return (
+                <div>
+                  <input value={this.state.peerTarget} placeholder='Connect to peer' onChange={this._changeField} />
+                  <button onClick={this._connectHandler}>Connect</button>
+                </div>
+            )
+        }
         var peerIdBlock = this.state.peerId ? this.state.peerId: ''
+        var connectTo = this.state.src ? renderConnect(): ''
 
         return (
             <div>
               <div className='video-container'>
-                <video className='their-video' autoplay></video>
+                <video className='their-video' autoPlay src={this.state.theirVideo}></video>
                 <video className='my-video' muted='true' autoPlay ref='myVideo' src={this.state.src}></video>
               </div>
               <div>
                 {peerIdBlock}
+                {connectTo}
               </div>
             </div>
          ) 
